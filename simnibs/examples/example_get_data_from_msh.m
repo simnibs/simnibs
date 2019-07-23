757bd18b482d1efc094b870ec5d380e1a297bfe4
guilhermebs <guilhermebs@drcmr.dk>
2019-07-23 11:17:47 +0200

    updated some matlab examples

--- simnibs/examples/example_get_data_from_msh.m
+++ simnibs/examples/example_get_data_from_msh.m
@@ -1,16 +1,11 @@
 %
-% Example script that reads a SimNIBS mesh with simulation results and
-% extracts the gray matter tetrahedra.
-%
-% To characterize the reached field strength, it determines the
-% peak magnitude (which might be affected by outliers) and some percentiles
-% of normE.
+% Example script that demonstrates how to access data in .msh-files
 % 
-% To characterize focality, it determines the GM volue in which the field
-% exceeds a certain fraction of the 99.9 percentile.
+% It reads a SimNIBS mesh with simulation results and reports some key values
+%       (1) for the whole cortex
+%       (2) in a sphere of 10 mm radius around the peak position
 %
-% Finally, it plots a histogram of the field values and the field on the
-% GM surface
+% It plots the field on the GM surface and the extracted region of interest
 %
 % A. Thielscher, 11-Apr-2018; updated 13-Sep-2018
 
@@ -18,74 +13,58 @@
 % load mesh
 [fname,pname] = uigetfile('*.msh','Select a mesh with simulation results');
 if isequal(fname,0) || isequal(pname,0); return; end
-m_head=mesh_load_gmsh4([pname fname]);
-
-% extract GM tetrahedra (GM has region number 2) and their data
-% and find data field containing the magnitude of E
-m=mesh_extract_regions(m_head,'tet',2);
-
-idxNormE=0;
-for i=1:length(m.element_data)
-    if strcmpi(m.element_data{i}.name,'normE')
-        idxNormE=i;
-        break;
-    end
-end
+m=mesh_load_gmsh4([pname fname]);
 
 
-% measures of field intensity
-%--------------------
-% get maximum field strength in GM
-normE=m.element_data{idxNormE}.tetdata;
-disp(' ');
-disp('---------------------------------- ');
-disp(['The peak field strength in GM is ' num2str(max(normE)) ' V/m']);
+% -------------------------------------------------------
+% EXAMPLE 1 
+% display some key results for whole cortex
+disp(' ')
+disp('whole cortex:')
+summary=mesh_get_fieldpeaks_and_focality(m,'field_idx',2);
 
-% get some percentiles of the field strength:
-% get tetrahedra volumes and sort the volumes using the 
-% field strength values to get the percentiles
-percentiles=[50 75 90 95 99 99.5 99.9];
-
-tetvols=mesh_get_tetrahedron_sizes(m);
-
-[normE,idx] = sort(normE);
-tetvols=tetvols(idx);
-tetvols=cumsum(tetvols);
-tetvolsNorm=tetvols/tetvols(end); % normalize to one
-
-for i=1:length(percentiles)
-    idx=find(tetvolsNorm>percentiles(i)/100,1,'first');
-    normE_prc(i)=normE(idx);
-end
-
-disp(' ');
-disp('percentiles of normE in GM in V/m:')
-disp(num2str(percentiles));
-disp(num2str(normE_prc));
+% show field on the GM surface
+mesh_show_surface(m)
 
 
-% measure of focality
-%--------------------
-% GM volume in which the field exceeds 50%, 75% and 90% of the 99.9
-% percentile
-cutoffs=[50 75 90];
-for i=1:length(cutoffs)
-    idx=find(normE>cutoffs(i)/100*normE_prc(end),1,'first');
-    GMvol_cutoff(i)=tetvols(end)-tetvols(idx);
-end
+% -------------------------------------------------------
+% EXAMPLE 2
+% extract a spherical ROI with 10 mm radius around the peak position
+%
+% the "summary" structure contains the peak values
+% together with their positions:
+%   "summary.percentiles" lists the tested percentile cutoffs - 
+%   the 99.9 percentile is the 3rd entry
+%
+%   "summary.perc_values" lists the corresponding values, as they
+%   are also displayed by mesh_get_fieldpeaks_and_focality
+%
+%   "summary.XYZ_perc" lists the corresponding center positions -
+%   the center position for the 99.9 percentile is the 3rd row:
+peak_pos=summary.XYZ_perc(3,:);
 
-disp(' ');
-disp('GM volume (in mm³), in which normE exceeds the');
-disp(['stated fractions of the ' num2str(percentiles(end)) ' percentile']);
-disp(num2str(cutoffs));
-disp(num2str(GMvol_cutoff,'%.0f'));
-disp('---------------------------------------- ');
-disp(' ');
+% distance to peak position
+dist=sqrt(sum(bsxfun(@minus,m.nodes,peak_pos).^2,2));
 
-% plot histogram of field in GM volume and show field on GM surface
-%--------------------
-figure;
-h = subplot(1,2,1);
-mesh_get_histogram(m,'haxis',h)
-h = subplot(1,2,2);
-mesh_show_surface(m_head,'haxis',h)
+% extract nodes closer than 10 mm, and belonging to tetrahedra with region
+% number 2 (gray matter has region number 2)
+node_idx=dist<10;
+m_ROI=mesh_extract_regions(m, 'elemtype','tet','region_idx',2,'node_idx', node_idx);
+
+% get some key results for the spherical ROI
+disp(' ')
+disp('10 mm spherical ROI around peak position:')
+mesh_get_fieldpeaks_and_focality(m_ROI,'field_idx',2);
+
+% show the extracted ROI:
+% show the whole cortex semi-transparent
+mesh_show_surface(m,'showSurface',true,'facealpha',0.4);
+% create new mesh containing the enclosing surface of the ROI tetrahedra
+TR = triangulation(m_ROI.tetrahedra,m_ROI.nodes(:,1),m_ROI.nodes(:,2),m_ROI.nodes(:,3));
+m_vis=mesh_empty;
+m_vis.nodes=m_ROI.nodes;
+m_vis.triangles=freeBoundary(TR);
+m_vis.triangle_regions=ones(size(m_vis.triangles,1),1);
+% add the he enclosing surface to the plot
+mesh_show_surface(m_vis,'showSurface',true,'surfaceColor',[1 0 0],'haxis',gca); 
+title('10 mm spherical ROI around peak position');
