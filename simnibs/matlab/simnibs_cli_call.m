761a491ff34cde3ab6e2a29e7a424eeef89daf19
Guilherme B Saturnino <guilhermebs@drcmr.dk>
2019-10-18 11:32:26 -0400

    added some scaping for spaces in matlab calls

--- simnibs/matlab/simnibs_cli_call.m
+++ simnibs/matlab/simnibs_cli_call.m
@@ -1,4 +1,4 @@
 function call = simnibs_cli_call(command)
  % Creates a call to a a SimNBIS CLI command
- call = [SIMNIBSPYTHON ' ' SIMNIBSDIR filesep 'cli' filesep command '.py'];
+ call = [SIMNIBSPYTHON ' ' '"' SIMNIBSDIR filesep 'cli' filesep command '.py' '"'];
 end
