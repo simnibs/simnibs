cd1f31592322908f77ec30f8ea53daf89a72e147
axelt <axelt@drcmr.dk>
2020-02-05 18:19:40 +0100

    deleting obsolete stuff

--- simnibs/resources/spm12/@slover/subsasgn.m
+++
@@ -1,9 +0,0 @@
-function result = subsasgn(this, Struct, rhs)
-% Method to overload . notation in assignments.
-% . assignment works directly on object fields
-%__________________________________________________________________________
-
-% Matthew Brett
-% $Id: subsasgn.m 6623 2015-12-03 18:38:08Z guillaume $
-
-result = builtin('subsasgn', this, Struct, rhs);
--- simnibs/resources/spm12/toolbox/DEM/expmall.m
+++
@@ -1,8 +0,0 @@
-function dx = expmall(J,f,t,EP)
-
-EP(EP==1) = J;
-EP(EP==2) = f;
-
-
-dx = expm(EP*t);
-
