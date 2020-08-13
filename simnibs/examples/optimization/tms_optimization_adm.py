a146e1d0cd614a566b9864043331628e6058910f
guilhermebs <guilhermebs@drcmr.dk>
2020-08-13 09:06:02 +0200

    added ADM tms optimization examples

---
+++ simnibs/examples/optimization/tms_optimization_adm.py
@@ -0,0 +1,17 @@
+from simnibs import opt_struct
+
+# Initialize structure
+tms_opt = opt_struct.TMSoptimize()
+# Select the head mesh
+tms_opt.fnamehead = 'ernie.msh'
+# Select output folder
+tms_opt.pathfem = 'tms_optimization/'
+# Select the coil model
+# The ADM method requires a '.ccd' coil model
+tms_opt.fnamecoil = 'Magstim_70mm_Fig8.ccd'
+# Select a target for the optimization
+tms_opt.target = [-43.4, -20.7, 83.4]
+# Use the ADM method
+tms_opt.method = 'ADM'
+# Run optimization
+tms_opt.run()
