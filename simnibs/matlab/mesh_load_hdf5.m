252fe5257ac7751dda9986748f8fb8e073432bf9
axelt <axelt@drcmr.dk>
2020-12-15 19:08:00 +0100

    added reading of leadfields from hdf5 for matlab

--- simnibs/matlab/mesh_load_hdf5.m
+++ simnibs/matlab/mesh_load_hdf5.m
@@ -1,5 +1,5 @@
 function meshes = mesh_load_hdf5(varargin)
-% Load meshes stored in hdf5 format
+% Load meshes and leadfields stored in hdf5 format
 %
 % meshes=mesh_load_hdf5(fn); Reads all the mesh structures in the root of
 % an hdf5 file named 'fn' and returns a struct with all mesh structures
@@ -8,18 +8,20 @@ function meshes = mesh_load_hdf5(varargin)
 % group_name and returns a single mesh structure
 
 % Guilherme Saturnino, 2018
+% Axel Thielscher, 15-Dec-2020: added reading of leadfields
 
 if nargin==2
     fn = varargin{1};
     group_name = varargin{2};
-    meshes = read_group_mesh(fn, group_name);
+    meshes.name = group_name;
+    [meshes.mesh, meshes.lf] = read_group_mesh(fn, group_name);
 elseif nargin==1
     fn = varargin{1};
     meshes = struct('name', {}, 'mesh', {});
     % Read the mesh in the root of the file, if any
     if is_mesh_struct(fn, '/')
         meshes(end+1).name = '/';
-        meshes(end).mesh = read_group_mesh(fn, '/'); 
+        [meshes(end).mesh, meshes(end).lf] = read_group_mesh(fn, '/'); 
     end
     % Scan the groups in the root and find the meshes
     ifo = h5info(fn);
@@ -27,7 +29,7 @@ elseif nargin==1
         g_name = ifo.Groups(i).Name;
         if is_mesh_struct(fn, g_name)
             meshes(end+1).name = g_name;
-            meshes(end).mesh = read_group_mesh(fn, g_name);
+            [meshes(end).mesh, meshes(end).lf]= read_group_mesh(fn, g_name);
         end
     end
     if isempty(meshes)
@@ -56,7 +58,7 @@ function v = is_mesh_struct(fn, group_name)
     end
 end
 
-function m = read_group_mesh(fn, group_name)
+function [m, lf] = read_group_mesh(fn, group_name)
     % Initialize structure
     m.node_data = {};
     m.element_data = {};
@@ -91,6 +93,33 @@ function m = read_group_mesh(fn, group_name)
         m.element_data{i}.tridata = double(data(elm_types==2, 1:end));
         m.element_data{i}.tetdata = double(data(elm_types==4, 1:end));
     end
+    % read Leadfields
+    lf=[];
+    ifo = h5info(fn, group_name);
+    idx=find(strcmp({ifo.Groups.Name},[group_name '/leadfields']), 1);
+    if ~isempty(idx)
+        ifo = h5info(fn, [group_name '/leadfields']);
+        if length(ifo.Datasets) > 1
+            error('more than one leadfield found ... ');
+        end
+        if ~isempty(ifo.Datasets)
+            dset = ifo.Datasets;
+            lf.name = dset.Name;
+            lf.data = h5read(fn, [group_name, '/leadfields/' dset.Name]);
+            % read leadfield properties
+            lf.properties = struct();
+            for j=1:length(dset.Attributes)
+                if iscell(dset.Attributes(j).Value)&&length(dset.Attributes(j).Value)==1
+                   lf.properties(1).(dset.Attributes(j).Name) = ...
+                        dset.Attributes(j).Value{1};
+                else
+                    lf.properties(1).(dset.Attributes(j).Name) = ...
+                        dset.Attributes(j).Value;
+                end
+            end
+        end  
+    end
+
 end
 
 function vout=ensure_horizontal(v)
