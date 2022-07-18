9e27c8f65fba33f32992e442a17856b4f49420cb
Jesper Duemose Nielsen <jdue@dtu.dk>
2022-07-18 14:24:22 +0200

    added command line tools for computing eeg forward solutions

---
+++ simnibs/cli/prepare_eeg_montage.py
@@ -0,0 +1,57 @@
+import argparse
+from pathlib import Path
+import sys
+
+from simnibs.cli.utils import clargs, clpars
+
+
+def parse_args(argv):
+
+    program = dict(
+        description = """
+            Convert electrode information from MNE-Python or FieldTrip data
+            structure to SimNIBS format. The resulting electrode positions
+            should be in MRI space and is writting to a CSV file.
+        """,
+    )
+
+    montage = dict(
+        type = str,
+        help = """Name of resulting EEG montage file. `csv` extension is
+        forced."""
+    )
+
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument("montage", **montage)
+
+    parser = argparse.ArgumentParser(**program)
+    sp_eeg = parser.add_subparsers(**clpars.read_format_eeg.kwargs)
+
+    # MNE
+    sp_mne = clpars.read_format_mne.add_to(sp_eeg, parents=[parent_parser])
+    clargs.info_mne.add_to(sp_mne)
+    clargs.trans_mne.add_to(sp_mne)
+
+    # FieldTrip
+    sp_fieldtrip = clpars.read_format_fieldtrip.add_to(sp_eeg, parents=[parent_parser])
+    clargs.info_fieldtrip.add_to(sp_fieldtrip)
+    clargs.trans_fieldtrip.add_to(sp_fieldtrip)
+
+    return parser.parse_args(argv[1:])
+
+
+if __name__ == '__main__':
+    args = parse_args(sys.argv)
+
+    montage = Path(args.montage).with_suffix(".csv")
+    info = Path(args.info)
+    trans = Path(args.trans)
+
+    if args.format == 'mne':
+        from simnibs.simulation.eeg_mne_tools import prepare_montage
+        prepare_montage(montage, info, trans)
+    elif args.format == 'fieldtrip':
+        from simnibs.simulation.eeg_fieldtrip_tools import prepare_montage
+        prepare_montage(montage, info, trans)
+    else:
+        raise ValueError(f'{args.format} is not a valid format.')
---
+++ simnibs/cli/prepare_tdcs_leadfield.py
@@ -0,0 +1,58 @@
+import argparse
+from pathlib import Path
+import sys
+
+from simnibs.cli.utils import clargs
+
+from simnibs.simulation.eeg import compute_tdcs_leadfield
+
+def parse_args(argv):
+
+    program = dict(
+        description = """
+            Convenience function to run a TDCS simulation..
+            This will compute the electric field and the central gray matter
+            surface. Electrodes are modeled as circular with diameter of 10 mm
+            and a thickness of 4 mm. Default conductivities will be used.
+
+            For more control, use the python function
+                simnibs.simulation.eeg.compute_tdcs_leadfield_for_eeg
+            or define the simulation from scratch using
+                simnibs.simulation.sim_struct.TDCSLEADFIELD.
+            """,
+    )
+
+    montage = dict(
+        type = str,
+        help = """Name of EEG montage file defining the electrode positions."""
+    )
+
+    # Optional
+
+    output_dir = dict(
+        type = str,
+        default = "fem_{subid}",
+        help = """Directory in which to store the results of the simulation. If
+            it does not exist, it will be created (default: %(default)s)."""
+    )
+
+
+    parser = argparse.ArgumentParser(**program)
+
+    clargs.subid.add_to(parser)
+    clargs.subsampling.add_to(parser)
+
+    parser.add_argument("montage", **montage)
+    parser.add_argument("-o", "--output_dir", **output_dir)
+
+    return parser.parse_args(argv[1:])
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv)
+
+    m2m_dir = clargs.resolve_subject_id_path(args.subid)
+    fem_dir = Path(args.outpur_dir).resolve()
+    montage = Path(args.montage)
+
+    compute_tdcs_leadfield(m2m_dir, fem_dir, montage, args.subsampling)
