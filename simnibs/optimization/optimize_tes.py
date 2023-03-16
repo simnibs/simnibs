[FIX] fixed problems for stable geodesic distance calculation in ellipsoid.py added Dirichlet BC estimation for fast TES calculations extended array_layout.py with additional parameters for Dirichlet BC

simnibs/optimization/optimize_tes.py
@@ -84,26 +84,6 @@ class TESoptimize():
         Areas of nodes
     node_idx_msh : np.ndarray of int [n_nodes_skin]
         Indices of skin surface nodes in global msh
-
-
-
-    max_total_current: float (optional)
-        Maximum current across all electrodes (in Amperes). Default: 2e-3
-    max_individual_current: float
-        Maximum current for any single electrode (in Amperes). Default: 1e-3
-    max_active_electrodes: int
-        Maximum number of active electrodes. Default: no maximum
-
-
-
-    name: str
-        Name for the optimization problem. Defaults tp 'optimization'
-    target: list of TDCStarget objects
-        list of TDCStarget objects defining the targets of the optimization
-    avoid: list of TDCSavoid objects (optional)
-        list of TDCSavoid objects defining regions to avoid
-    open_in_gmsh: bool (optional)
-        Whether to open the result in Gmsh after the calculations. Default: False
     """
 
     def __init__(self,
@@ -542,7 +522,8 @@ class TESoptimize():
     def get_nodes_electrode(self, electrode_pos, plot=False):
         """
         Assigns the skin points of the electrodes in electrode array and writes the points in
-        electrode_array.electrodes[i].nodes and electrode_array.electrodes[i].node_area
+        electrode_array.electrodes[i].nodes and electrode_array.electrodes[i].node_area.
+        Estimate optimal electrode currents based on previous simulations.
 
         Parameters
         ----------
@@ -626,7 +607,8 @@ class TESoptimize():
                                                                                   surface=self.skin_surface)
 
             if len(ele_idx) != len(alpha):
-                return "Electrode position: invalid (not all electrodes in valid skin region)"
+                # return "Electrode position: invalid (not all electrodes in valid skin region)"
+                print("Electrode position: invalid (not all electrodes in valid skin region)")
 
         # i_ele = 0
         # ele_idx_rect = []
@@ -693,10 +675,10 @@ class TESoptimize():
                         return "Electrode position: invalid (partly overlaps with invalid skin region)"
 
                     # save node indices (refering to global mesh)
-                    _electrode.node_idx = self.node_idx_msh[np.where(mask)[0]]
+                    _electrode.node_idx = self.node_idx_msh[mask]
 
                     # save node coords (refering to global mesh)
-                    _electrode.node_coords = self.mesh.nodes.node_coord[np.where(mask)[0]]
+                    _electrode.node_coords = self.skin_surface.nodes[mask]
 
                     # save number of nodes assigned to this electrode
                     _electrode.n_nodes = len(_electrode.node_idx)
@@ -732,13 +714,49 @@ class TESoptimize():
 
         if plot:
             # np.savetxt(os.path.join(self.plot_folder, "electrode_coords_center_ellipsoid.txt"), electrode_coords_eli_cart)
-            # np.savetxt(os.path.join(self.plot_folder, "electrode_coords_center_subject.txt"), electrode_coords_subject)
+            # np.savetxt(os.path.join(self.plot_folder, "electrode_coords_center_subject.txt"), electrode_coords_subject[0])
+            # np.savetxt(os.path.join(self.plot_folder, "electrode_coords_nodes_subject.txt"), node_coords_list[0])
+            # np.savetxt(os.path.join(self.plot_folder, "electrode_coords_nodes_subject.txt"), self.mesh.nodes.node_coord[self.node_idx_msh])
+            # np.savetxt(os.path.join(self.plot_folder, "electrode_coords_nodes_subject.txt"), self.mesh.nodes.node_coord[self.electrode[0].electrode_arrays[0].electrodes[0].node_idx])
             for i_channel_stim in range(self.n_channel_stim):
                 node_idx_all = np.hstack([np.hstack(node_idx_dict[i_channel_stim][id])
                                           for id in node_idx_dict[i_channel_stim].keys()])
                 points_nodes = self.mesh.nodes.node_coord[node_idx_all, :]
                 np.savetxt(os.path.join(self.plot_folder, f"electrode_coords_nodes_subject_{i_channel_stim}.txt"),
                            points_nodes)
+
+        # save electrode_pos in ElectrodeArray instances
+        for i_channel_stim in range(self.n_channel_stim):
+            for i_array, _electrode_array in enumerate(self.electrode[i_channel_stim].electrode_arrays):
+                _electrode_array.electrode_pos = electrode_pos[i_channel_stim][i_array]
+
+        # estimate optimal electrode currents based on previous simulations
+        # current_pos = 0
+        # current_neg = 0
+        # for i_channel_stim in range(self.n_channel_stim):
+        #     for i_array, _electrode_array in enumerate(self.electrode[i_channel_stim].electrode_arrays):
+        #         for _electrode in _electrode_array.electrodes:
+        #             _electrode.estimate_currents(electrode_pos[i_channel_stim])
+        #
+        #             # sum up current for scaling
+        #             # estimator does not know about real total current and has to be corrected
+        #             if _electrode.current < 0:
+        #                 current_neg += _electrode.current
+        #             else:
+        #                 current_pos += _electrode.current
+        #
+        # scale current to match total current
+        # current_temp = []
+        # for i_channel_stim in range(self.n_channel_stim):
+        #     for i_array, _electrode_array in enumerate(self.electrode[i_channel_stim].electrode_arrays):
+        #         for _electrode in _electrode_array.electrodes:
+        #             if _electrode.current < 0:
+        #                 _electrode.current = _electrode.current/np.abs(current_neg) * self.electrode[i_channel_stim].current_total
+        #             else:
+        #                 _electrode.current = _electrode.current/np.abs(current_pos) * self.electrode[i_channel_stim].current_total
+        #             current_temp.append(_electrode.current)
+
+        # self.logger.log(20, f'Estimating currents: { *current_temp, }')
 
         return node_idx_dict
 
@@ -761,7 +779,7 @@ class TESoptimize():
 
         e = [[0 for _ in range(self.n_roi)] for _ in range(self.n_channel_stim)]
 
-        # assign surface nodes to electrode positions
+        # assign surface nodes to electrode positions and estimate optimal currents
         # start = time.time()
         node_idx_dict = self.get_nodes_electrode(electrode_pos=electrode_pos, plot=plot)
         # stop = time.time()
@@ -777,15 +795,6 @@ class TESoptimize():
             # set RHS
             b = self.ofem.set_rhs(electrode=self.electrode[i_channel_stim])
 
-            #start = time.time()
-            # b = self.ofem.fem.assemble_tdcs_neumann_rhs(electrodes=[node_idx_dict[i_channel_stim][channel]
-            #                                                         for channel in node_idx_dict[i_channel_stim].keys()],
-            #                                             currents=self.current[i_channel_stim],
-            #                                             input_type='node',
-            #                                             areas=self.mesh_nodes_areas)
-
-
-
             # solve system
             if self.dirichlet_correction[i_channel_stim]:
                 v = self.ofem.solve_dirichlet_correction(b=b, electrode=self.electrode[i_channel_stim])
@@ -795,7 +804,11 @@ class TESoptimize():
             # Determine electric field in ROIs
             #start = time.time()
             for i_roi, r in enumerate(self.roi):
-                 e[i_channel_stim][i_roi] = r.calc_fields(v)
+                if v is None:
+                    e[i_channel_stim][i_roi] = None
+                    self.logger.log(20, "Warning! Simulation failed! Returning e-field: None!")
+                else:
+                    e[i_channel_stim][i_roi] = r.calc_fields(v)
             #stop = time.time()
             #print(f"Time: calc fields: {stop - start}")
 
@@ -803,19 +816,20 @@ class TESoptimize():
             if plot:
                 for j in range(self.n_channel_stim):
                     for i, _e in enumerate(e[j]):
-                        import pynibs
-                        if not os.path.exists(os.path.join(self.plot_folder, f"e_roi_{i}_geo.hdf5")):
+                        if _e is not None:
+                            import pynibs
+
                             pynibs.write_geo_hdf5_surf(out_fn=os.path.join(self.plot_folder, f"e_roi_{i}_geo.hdf5"),
                                                        points=self.roi[i].points,
                                                        con=self.roi[i].con,
                                                        replace=True,
                                                        hdf5_path='/mesh')
 
-                        pynibs.write_data_hdf5_surf(data=[np.mean(_e.flatten()[self.roi[i].con], axis=1)],
-                                                    data_names=["E_mag"],
-                                                    data_hdf_fn_out=os.path.join(self.plot_folder, f"e_stim_{j}_roi_{i}_data.hdf5"),
-                                                    geo_hdf_fn=os.path.join(self.plot_folder, f"e_roi_{i}_geo.hdf5"),
-                                                    replace=True)
+                            pynibs.write_data_hdf5_surf(data=[np.mean(_e.flatten()[self.roi[i].con], axis=1)],
+                                                        data_names=["E_mag"],
+                                                        data_hdf_fn_out=os.path.join(self.plot_folder, f"e_stim_{j}_roi_{i}_data.hdf5"),
+                                                        geo_hdf_fn=os.path.join(self.plot_folder, f"e_roi_{i}_geo.hdf5"),
+                                                        replace=True)
 
         return e
 
@@ -898,11 +912,17 @@ class TESoptimize():
             if self.goal == "mean":
                 for i_channel_stim in range(self.n_channel_stim):
                     for i_roi in range(self.n_roi):
-                        y[i_channel_stim, i_roi] = -np.mean(e[i_channel_stim][i_roi])
+                        if e[i_channel_stim] is None:
+                            y[i_channel_stim, i_roi] = 1
+                        else:
+                            y[i_channel_stim, i_roi] = -np.mean(e[i_channel_stim][i_roi])
             elif self.goal == "max":
                 for i_channel_stim in range(self.n_channel_stim):
                     for i_roi in range(self.n_roi):
-                        y[i_channel_stim, i_roi] = -np.percentile(e[i_channel_stim][i_roi], 99.9)
+                        if e[i_channel_stim] is None:
+                            y[i_channel_stim, i_roi] = 1
+                        else:
+                            y[i_channel_stim, i_roi] = -np.percentile(e[i_channel_stim][i_roi], 99.9)
             else:
                 raise NotImplementedError(f"Specified goal: '{self.goal}' not implemented as goal function.")
 

simnibs/optimization/tests/test_optimize_tes.py
@@ -10,6 +10,7 @@ import os
 import h5py
 import pynibs
 import simnibs
+import nibabel
 import numpy as np
 import matplotlib.pyplot as pltPowd9Twy
 
@@ -33,23 +34,23 @@ example_data_folder = os.path.join(simnibs.SIMNIBSDIR, '_internal_resources', 't
 print("Initializing Electrode ...")
 # create a circular array with 1 center electrode and 6 outer electrodes
 ########################################################################################################################
-electrode = simnibs.CircularArray(radius_inner=6, distance=20, n_outer=4, radius_outer=5)
+# electrode = simnibs.CircularArray(radius_inner=6, distance=20, n_outer=4, radius_outer=5)
 
 # create 3 x 3 circular electrode array pair
 ########################################################################################################################
-# center = np.array([[-30, 20, 0],
-#                     [0, 20, 0],
-#                     [30, 20, 0],
-#                     [-30, 0, 0],
-#                     [0, 0, 0],
-#                     [30, 0, 0],
-#                     [-30, -20, 0],
-#                     [0, -20, 0],
-#                     [30, -20, 0]])
-# radius = np.array([7, 7, 7, 7, 7, 7, 7, 7, 7])
-# length_x = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# length_y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# electrode = simnibs.ElectrodeArrayPair(center=center, radius=radius, length_x=length_x, length_y=length_y)
+center = np.array([[-30, 20, 0],
+                    [0, 20, 0],
+                    [30, 20, 0],
+                    [-30, 0, 0],
+                    [0, 0, 0],
+                    [30, 0, 0],
+                    [-30, -20, 0],
+                    [0, -20, 0],
+                    [30, -20, 0]])
+radius = np.array([7, 7, 7, 7, 7, 7, 7, 7, 7])
+length_x = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+length_y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+electrode = simnibs.ElectrodeArrayPair(center=center, radius=radius, length_x=length_x, length_y=length_y)
 # electrode.electrode_arrays[0].plot(show=False, fn_plot=os.path.join(output_folder, "plots", "electrode.png"))
 
 # create two 3 x 3 circular electrode array pairs (2 channels)
@@ -97,6 +98,14 @@ with h5py.File(fn_roi, "r") as f:
     points = f["mesh/nodes/node_coord"][:]
     con = f["mesh/elm/triangle_number_list"][:]
 
+# lh = nibabel.load("/data/pt_01756/probands/15484.08/mesh/charm_beta_coarse/m2m_15484.08/surf/lh.central.gii")
+# rh = nibabel.load("/data/pt_01756/probands/15484.08/mesh/charm_beta_coarse/m2m_15484.08/surf/rh.central.gii")
+# lh_points, lh_con = lh.agg_data()
+# rh_points, rh_con = rh.agg_data()
+# points = np.vstack((lh_points, rh_points))
+# con = np.vstack((lh_con, rh_con + lh_points.shape[0]))
+# tri_center = np.average(points[con, ], axis=1)
+
 # create a region of interest
 print("Initializing ROI ...")
 roi = simnibs.RegionOfInterest(points=points, con=con, mesh=mesh)
@@ -113,7 +122,7 @@ opt = simnibs.opt_struct.TESoptimize(mesh=mesh,
                                      electrode=electrode,
                                      init_pos=init_pos,
                                      output_folder=output_folder,
-                                     plot=False,
+                                     plot=True,
                                      optimizer="direct",
                                      optimizer_options=optimizer_options,
                                      min_electrode_distance=min_electrode_distance,

simnibs/simulation/array_layout.py
@@ -1,14 +1,20 @@
 import copy
 import numpy as np
+from .current_estimator import CurrentEstimator
 
 
 class Electrode():
-    """ Electrode class. Contains a single electrode. ElectrodeArrays are built from Electrode instances
+    """
+    Electrode class.
+    Contains a single circular or rectangular electrode.
+    ElectrodeArrays are built from Electrode instances.
 
     Parameters
     ----------
     channel_id : int
         Channel identifier, indicates connected electrodes (same identifier)
+    ele_id : int
+        Unique electrode identifier in whole setup
     center : np.ndarray of float [3]
         Center of electrode (in mm)
     radius : float, optional, default: None
@@ -58,7 +64,7 @@ class Electrode():
         Type of electrode ("spherical", "rectangular")
     """
 
-    def __init__(self, channel_id, center, radius=None, length_x=None, length_y=None, current=None, voltage=None):
+    def __init__(self, channel_id, ele_id, center, radius=None, length_x=None, length_y=None, current=None, voltage=None):
         if radius is None:
             radius = 0
 
@@ -69,11 +75,14 @@ class Electrode():
             length_y = 0
 
         self.channel_id = channel_id
+        self.ele_id = ele_id
         self.center = center
         self.radius = radius
         self.length_x = length_x
         self.length_y = length_y
         self.current = current
+        self.current_init = current
+        self.current_sign = np.sign(self.current_init)
         self.voltage = voltage
         self.transmat = None
         self.node_area = None
@@ -100,6 +109,9 @@ class Electrode():
             self.area = length_x * length_y
             self.type = "rectangular"
 
+        # initialize current estimator for fake Dirichlet BC
+        self.current_estimator = CurrentEstimator(electrode=self)
+
     def transform(self, transmat):
         """
         Transforms the electrode position matrix self.posmat_norm by the given 4x4 transformation matrix
@@ -112,6 +124,20 @@ class Electrode():
         """
         self.transmat = transmat
         self.posmat = self.posmat_norm @ transmat
+
+    def estimate_currents(self, electrode_pos):
+        """
+        Estimate electrode currents for fake Dirichlet BC using the CurrentEstimator class.
+        Writes current in self.current.
+
+        Parameters
+        ----------
+        electrode_pos : list of np.ndarray of float [n_array_free][n_pos x 3]
+            Positions and orientations of ElectrodeArrayPair or CircularArray
+        """
+        if self.current_estimator.current is None:
+            return
+        self.current = self.current_estimator.estimate_current(electrode_pos=np.hstack(electrode_pos))
 
 
 class ElectrodeArray():
@@ -129,6 +155,8 @@ class ElectrodeArray():
     channel_id : np.ndarray of int [n_ele]
         Channel identifiers, indicates connected electrodes (same identifier), in case of mixed definitions (circular
         and rectangular), the IDs of the circular electrodes are coming first followed by the rectangular ones.
+    ele_id : np.ndarray of int [n_ele]
+        Unique electrode identifier of whole setup
     center : np.ndarray of float [n_ele, 3]
         Center coordinates (x,y,z) of electrodes in normalized space (in mm)
     radius : np.ndarray of float [n_ele_circ] or None
@@ -172,10 +200,13 @@ class ElectrodeArray():
         Euclidean distances of electrodes to array center (0, 0, 0)
     angle : np.ndarray of float [n_ele]
         Angle between connection line electrodes <-> array center and principal direction axis of array (0, 1, 0)
+    electrode_pos : np.ndarray of float [3]
+        Position and orientation of electrode array in ellipsoidal coordinates [beta, lambda, alpha]
     """
 
-    def __init__(self, channel_id, center, radius=None, length_x=None, length_y=None, current=None):
+    def __init__(self, channel_id, ele_id, center, radius=None, length_x=None, length_y=None, current=None):
         self.channel_id = channel_id
+        self.ele_id = ele_id
         self.array_center = np.array([0, 0, 0])
         self.center = center
         self.radius = radius
@@ -191,6 +222,7 @@ class ElectrodeArray():
                                      [0, 0, 0, 1]])
         self.posmat = copy.deepcopy(self.posmat_norm)
         self.transmat = None
+        self.electrode_pos = None
 
         # apply Dirichlet correction step in fast TES calculations if electrodes share the same channel
         if self.n_ele != len(np.unique(self.channel_id)):
@@ -240,6 +272,7 @@ class ElectrodeArray():
                     self.angle[i_ele] = 0
 
             self.electrodes.append(Electrode(channel_id=self.channel_id[i_ele],
+                                             ele_id=self.ele_id[i_ele],
                                              center=self.center[i_ele, :],
                                              radius=self.radius[i_ele],
                                              length_x=self.length_x[i_ele],
@@ -301,7 +334,7 @@ class ElectrodeArray():
 
             ax.add_artist(obj)
             plt.annotate(f"Ch: {cha}", xy=cen[:2], xytext=(-12, 0), va='center',
-            xycoords = 'data', textcoords = 'offset points')
+            xycoords='data', textcoords='offset points')
 
         # plt.tight_layout()
         plt.grid()
@@ -364,7 +397,12 @@ class ElectrodeArrayPair():
         self.center = center
         self.n_ele = len(center) * 2                                                        # total number of electrodes
         self.channel_id = np.hstack([[i for _ in range(len(center))] for i in range(2)])    # array of all channel_ids
-        self.n_channel = np.unique(self.channel_id)
+        self.channel_id_unique = np.unique(self.channel_id)
+        self.n_channel = len(self.channel_id_unique)
+        self.ele_id = np.arange(self.n_ele)
+
+        if len(self.channel_id) != len(self.channel_id_unique):
+            self.dirichlet_correction = True
 
         if current is None:
             self.current = np.hstack((np.array([1/(self.n_ele/2.) for _ in range(int(self.n_ele/2))]),
@@ -391,8 +429,9 @@ class ElectrodeArrayPair():
                                                 radius=self.radius,
                                                 length_x=self.length_x,
                                                 length_y=self.length_y,
-                                                channel_id=self.channel_id[i],
-                                                current=self.current[i]) for i in range(2)]
+                                                channel_id=self.channel_id[self.channel_id == self.channel_id_unique[i]],
+                                                ele_id=self.ele_id[self.channel_id == self.channel_id_unique[i]],
+                                                current=self.current[self.channel_id == self.channel_id_unique[i]]) for i in range(2)]
 
     def update_geometry(self, center=None, radius=None, length_x=None, length_y=None):
         """
@@ -425,7 +464,9 @@ class ElectrodeArrayPair():
                                                 radius=self.radius,
                                                 length_x=self.length_x,
                                                 length_y=self.length_y,
-                                                channel_id=[i for _ in range(self.n_ele)]) for i in range(2)]
+                                                channel_id=self.channel_id[self.channel_id == self.channel_id_unique[i]],
+                                                ele_id=self.ele_id[self.channel_id == self.channel_id_unique[i]],
+                                                current=self.current[self.channel_id == self.channel_id_unique[i]]) for i in range(2)]
 
 
 class CircularArray():
@@ -464,8 +505,10 @@ class CircularArray():
         self.n_ele = n_outer + 1
         self.n_outer = n_outer
         self.channel_id = np.array([0] + [1] * n_outer)
-        self.n_channel = len(np.unique(self.channel_id))
+        self.channel_id_unique = np.unique(self.channel_id)
+        self.n_channel = len(self.channel_id_unique)
         self.dirichlet_correction = True
+        self.ele_id = np.arange(self.n_ele)
 
         if radius_outer is None:
             self.radius_outer = radius_inner
@@ -506,7 +549,9 @@ class CircularArray():
             if np.isclose(self.center[-1, 1], 0):
                 self.center[-1, 1] = 0.
 
+        # initialize freely movable electrode arrays
         self.electrode_arrays = [ElectrodeArray(channel_id=self.channel_id,
+                                                ele_id=self.ele_id,
                                                 center=self.center,
                                                 radius=self.radius,
                                                 length_x=self.length_x,
@@ -558,7 +603,10 @@ class CircularArray():
             self.center[-1, 1] = np.sin(i*(2*np.pi)/self.n_outer+np.pi/2) * self.distance
 
         self.electrode_arrays = [ElectrodeArray(channel_id=np.array([0] + [1] * self.n_outer),
+                                                ele_id=self.ele_id,
                                                 center=self.center,
                                                 radius=self.radius,
                                                 length_x=self.length_x,
-                                                length_y=self.length_y)]
+                                                length_y=self.length_y,
+                                                current=self.current)]
+

simnibs/simulation/onlinefem.py
@@ -9,6 +9,7 @@ import nibabel as nib
 from numpy.linalg import pinv
 from scipy import sparse
 from scipy.ndimage import zoom
+from scipy.optimize import minimize
 
 from .fem import FEMSystem, get_dirichlet_node_index_cog, DirichletBC, dofMap
 from .sim_struct import SimuList
@@ -121,7 +122,7 @@ class OnlineFEM:
         self.fn_tensor_nifti = self.ff_subject.tensor_file
 
         # get Dirichlet node index where V=0 (close to center of gravity of mesh but not on a surface)
-        self.dirichlet_node = mesh.nodes.nr  #  get_dirichlet_node_index_cog(mesh=self.mesh)
+        self.dirichlet_node = get_dirichlet_node_index_cog(mesh=self.mesh)  # self.mesh.nodes.node_number[self.mesh.nodes.node_coord[:, 2].argmin()] #   # mesh.nodes.nr
 
         # For TMS we use only tetrahedra (elm_type=4). The triangles (elm_type=3) are removed.
         if self.method == "TMS":
@@ -148,7 +149,7 @@ class OnlineFEM:
         if method == "TES" and electrode is None:
             raise AssertionError("Please provide TES electrode object for TES simulations.")
 
-    def update_field(self, electrodes=None, currents=None, matsimnibs=None, sim_idx_hdf5=0, didt=1e6):
+    def update_field(self, electrodes=None, matsimnibs=None, sim_idx_hdf5=0, didt=1e6):
         """
         Calculating and updating electric field for given coil position (matsimnibs) for TMS or electrode position (TES)
 
@@ -192,26 +193,24 @@ class OnlineFEM:
                 self.b = self.set_rhs(matsimnibs=matsimnibs[:, :, i_sim])
 
             elif self.method == "TES":
-                # if Dirichlet correction has to be done, use initial guess for currents if available
-                if self.dirichlet_correction[i_sim] and (self.currents_guess is not None):
-                    currents = self.currents_guess
-
-                self.b = self.set_rhs(electrodes=electrodes[i_sim],
-                                      currents=currents[i_sim])
+                self.b = self.set_rhs(electrode=electrodes[i_sim])
 
             # solve for potential
             ############################################################################################################
             if self.method == "TES" and self.dirichlet_correction[i_sim]:
                 self.v = self.solve_dirichlet_correction(b=self.b,
-                                                         electrode=self.electrode[i_sim],
-                                                         currents=currents[i_sim])
+                                                         electrode=self.electrode[i_sim])
             else:
                 self.v = self.solve(b=self.b)
 
             # calculate e-field
             ############################################################################################################
             for i_roi, r in enumerate(self.roi):
-                self.e[i_sim][i_roi] = r.calc_fields(v=self.v, dadt=self.dadt, dataType=self.dataType)  # * self.scale
+                if self.v is None:
+                    self.e[i_sim][i_roi] = None
+                    self.logger.log(20, "Warning! Simulation failed! Returning e-field: None!")
+                else:
+                    self.e[i_sim][i_roi] = r.calc_fields(v=self.v, dadt=self.dadt, dataType=self.dataType)
 
                 if self.method == "TMS":
                     self.e[i_sim][i_roi] *= didt * 1e-9
@@ -257,11 +256,11 @@ class OnlineFEM:
             currents = []
             for _electrode_array in electrode.electrode_arrays:
                 for _ele in _electrode_array.electrodes:
-                    electrodes.append(_ele.node_idx)
+                    electrodes.append(_ele.node_idx + 1)
                     currents.append(_ele.current)
 
-            b = self.fem.assemble_tdcs_neumann_rhs(electrodes=electrodes,
-                                                   currents=currents,
+            b = self.fem.assemble_tdcs_neumann_rhs(electrodes=electrodes,       # list of node indices of electrodes
+                                                   currents=currents,           # list of electrode currents
                                                    input_type='node',
                                                    areas=self.msh_nodes_areas)
 
@@ -355,6 +354,8 @@ class OnlineFEM:
 
             v_mean_ele[i] = np.array(v_mean_ele[i])
 
+        print(f"v_mean_ele: {v_mean_ele}")
+
         v_ele_norm = [np.zeros(electrode.n_ele_per_channel[i]) for i in range(electrode.n_channel)]
         currents_ele_norm = [np.zeros(electrode.n_ele_per_channel[i]) for i in range(electrode.n_channel)]
 
@@ -369,6 +370,142 @@ class OnlineFEM:
             currents_ele_norm[i_channel] = (currents[i_channel] - np.mean(currents[i_channel])) / np.mean(currents[i_channel])
 
         return v_ele_norm, currents_ele_norm
+
+    def optimize_currents(self, x, electrode, v_node_idx, channel_id_nodes, ele_id_nodes, node_area, signs):
+        """
+
+        :return:
+        """
+        channel_id_unique = np.unique(channel_id_nodes)
+        n_channel = len(channel_id_unique)
+
+        # write currents in electrode
+        ele_counter = 0
+        for _electrode_array in electrode.electrode_arrays:
+            for _ele in _electrode_array.electrodes:
+                _ele.current = x[ele_counter]
+                ele_counter += 1
+
+        # set rhs
+        b = self.set_rhs(electrode=electrode)
+
+        # solve
+        self.v = self.solve(b)
+        v_nodes = self.v[v_node_idx]
+
+        # extract and average voltages of channels and electrodes
+        # average and weight potential with node area over electrodes and whole channel (separately)
+        v_mean_ele = []  # [n_channel][n_ele]
+        v_mean_channel = []  # [n_channel]
+        v_var_channel = np.zeros(len(channel_id_unique))   # [n_channel]
+
+        for i, _channel_id in enumerate(channel_id_unique):
+            channel_mask = _channel_id == channel_id_nodes
+
+            # average area weighted potential over whole channel
+            v_mean_channel.append(np.sum(node_area[channel_mask] * v_nodes[channel_mask]) / \
+                                  np.sum(node_area[channel_mask]))
+
+            ele_id_unique = np.unique(ele_id_nodes[channel_mask])
+
+            # for this channel determine average potentials over electrodes
+            v_mean_ele.append([])
+            for _ele_id in ele_id_unique:
+                ele_mask = _ele_id == ele_id_nodes
+                mask = ele_mask * channel_mask
+                v_mean_ele[i].append(np.sum(node_area[mask] * v_nodes[mask]) / \
+                                     np.sum(node_area[mask]))
+
+            v_mean_ele[i] = np.array(v_mean_ele[i])
+            v_var_channel[i] = np.var(v_mean_ele[i])
+
+        # evaluate goal function (variance of channel voltages over electrodes)
+        var_v = np.linalg.norm(v_var_channel)
+
+        print(x,var_v)
+
+        return var_v
+
+    def current_constraint(self, x, electrode, v_node_idx, channel_id_nodes, ele_id_nodes, node_area, signs):
+        current_pos_mask = np.where(signs > 0)[0]
+        current_neg_mask = np.where(signs < 0)[0]
+
+        c = np.abs(np.sum(x[current_pos_mask]) - electrode.current_total) + \
+            np.abs(np.sum(np.abs(x[current_neg_mask])) - electrode.current_total) + \
+            np.abs(np.sum(x))
+
+        # c = np.sum(np.abs(x)) - 2*electrode.current_total
+
+        return c
+
+    # def solve_dirichlet_correction(self, b, electrode):
+    #     """
+    #
+    #     :param electrode:
+    #     :return:
+    #     """
+    #     # create arrays
+    #     # v_nodes | v_node_idx | channel_id_nodes | ele_id_nodes | node_area
+    #     # ------------------------------------------------------------------
+    #     #   ...         4               0                 0           ...
+    #     #   ...         1               0                 0           ...
+    #     #   ...         15              0                 1           ...
+    #     #   ...         3               0                 1           ...
+    #     #   ...         9               1                 0           ...
+    #     #   ...         8               1                 0           ...
+    #     #   ...         22              1                 0           ...
+    #     #   ...         0               1                 1           ...
+    #     #   ...         10              1                 1           ...
+    #     #   ...         11              1                 1           ...
+    #
+    #     n_channel = electrode.n_channel
+    #     channel_id_nodes = []
+    #     ele_id_nodes = []
+    #     v_node_idx = []
+    #     node_area = []
+    #
+    #     for _electrode_array in electrode.electrode_arrays:
+    #         for i_ele, _ele in enumerate(_electrode_array.electrodes):
+    #             channel_id_nodes.append(np.array([_ele.channel_id] * _ele.n_nodes))
+    #             ele_id_nodes.append(np.array([i_ele] * _ele.n_nodes))
+    #             node_area.append(_ele.node_area)
+    #             v_node_idx.append(_ele.node_idx)
+    #
+    #     channel_id_nodes = np.hstack(channel_id_nodes)
+    #     ele_id_nodes = np.hstack(ele_id_nodes)
+    #     node_area = np.hstack(node_area)
+    #     v_node_idx = np.hstack(v_node_idx)
+    #
+    #     # set initial values and bounds
+    #     x0 = [np.zeros(electrode.n_ele_per_channel[i_channel]) for i_channel in range(n_channel)]
+    #     bounds = [[] for i_channel in range(n_channel)]
+    #
+    #     ele_counter_channel = [0] * n_channel
+    #     for _electrode_array in electrode.electrode_arrays:
+    #         for _ele in _electrode_array.electrodes:
+    #             x0[_ele.channel_id][ele_counter_channel[_ele.channel_id]] = _ele.current_init
+    #             bounds[_ele.channel_id].append(np.sort(
+    #                 [_ele.current_init * 0.1,
+    #                  np.sign(electrode.current_mean[_ele.channel_id]) * electrode.current_total]))
+    #             ele_counter_channel[_ele.channel_id] += 1
+    #     bounds = sum(bounds, [])
+    #     x0 = np.hstack(x0).flatten()
+    #     signs = np.sign(x0)
+    #     self.logger.log(20, f"Starting current optimization with x0: {x0}")
+    #
+    #     # setup optimization algorithm
+    #     res = minimize(self.optimize_currents,
+    #                    x0=x0,
+    #                    bounds=bounds,
+    #                    args=(electrode, v_node_idx, channel_id_nodes, ele_id_nodes, node_area, signs),
+    #                    method="trust-constr",  #"SLSQP",
+    #                    constraints={"fun": self.current_constraint, "type": "eq", "args": (electrode, v_node_idx, channel_id_nodes, ele_id_nodes, node_area, signs)},
+    #                    jac=None, hess=None, hessp=None, tol=None, callback=None, options={"gtol": 1e-5, "xtol":1e-5})
+    #     if res.success:
+    #         self.logger.log(20, f"Optimized currents: {res.x}")
+    #         return self.v
+    #     else:
+    #         return None
 
     def solve_dirichlet_correction(self, b, electrode):
         """
@@ -388,7 +525,7 @@ class OnlineFEM:
             Corrected solution (including the Dirichlet node at the right position)
         """
         th_maxrelerr = 0.01
-        maxiter = 100
+        maxiter = 40
 
         # create arrays
         # v_nodes | v_node_idx | channel_id_nodes | ele_id_nodes | node_area
@@ -425,18 +562,26 @@ class OnlineFEM:
         # mean current over electrodes [n_channel]
         I_mean = electrode.current_mean
 
-        # Solve iteratively until maxrelerr is reached
-        # Iteration: 0
-        v = self.solve(b)
-        v_nodes = v[v_node_idx]
-
         # read currents from electrode
         I = [np.zeros(electrode.n_ele_per_channel[i_channel]) for i_channel in range(n_channel)]
+        I_sign = [np.zeros(electrode.n_ele_per_channel[i_channel]) for i_channel in range(n_channel)]
+        bounds = [[] for i_channel in range(n_channel)]
         ele_counter_channel = [0] * n_channel
         for _electrode_array in electrode.electrode_arrays:
             for _ele in _electrode_array.electrodes:
                 I[_ele.channel_id][ele_counter_channel[_ele.channel_id]] = _ele.current
+                I_sign[_ele.channel_id][ele_counter_channel[_ele.channel_id]] = _ele.current_sign
+                bounds[_ele.channel_id].append(np.sort(
+                                    [_ele.current * 0.1,
+                                     np.sign(electrode.current_mean[_ele.channel_id]) * electrode.current_total]))
                 ele_counter_channel[_ele.channel_id] += 1
+
+        # Solve iteratively until maxrelerr is reached
+        # Iteration: 0
+
+        # solve
+        v = self.solve(b)
+        v_nodes = v[v_node_idx]
 
         # v_norm: [n_channel][n_ele], I_norm: [n_channel][n_ele]
         v_norm, I_norm = self.normalize_solution(v_nodes=v_nodes,
@@ -453,13 +598,12 @@ class OnlineFEM:
         j = 0
         maxrelerr = np.max([np.max(np.abs(v_norm[k][-1])) for k in range(n_channel)])
         while maxrelerr > th_maxrelerr:
-            print(f"iter: {j} error: {maxrelerr}")
+            print(f"iter: {j} current: {I}, error: {maxrelerr}")
             j += 1
 
             if j > maxiter:
                 print('warning: did not converge after ' + str(maxiter) + ' iterations')
-                j -= 1  # hack to make visualization happy
-                break
+                return None
 
             if j == 1:
                 # It 1: make small step hopefully in the right direction
@@ -469,6 +613,7 @@ class OnlineFEM:
                         I.append(np.array([I_mean[i_channel]]))
                     else:
                         I.append(-0.1 * np.sign(I_mean[i_channel]) * v_norm[i_channel][0, :] / np.max(np.abs(v_norm[i_channel][0, :])))
+
             else:
                 # It 2 ... use gradient descent
                 for i_channel in range(n_channel):
@@ -476,9 +621,8 @@ class OnlineFEM:
                         I[i_channel] = I[i_channel]
                     else:
                         denom = (v_norm[i_channel][-1, :] - v_norm[i_channel][-2, :])
-                        # denom += np.sign(denom) * maxrelerr / 15  # regularize: avoid too small denominators to gain stability
+                        denom += np.sign(denom) * maxrelerr / 15  # regularize: avoid too small denominators to gain stability
                         I[i_channel] = I_norm[i_channel][-1] - (I_norm[i_channel][-1, :] - I_norm[i_channel][-2, :]) / denom * v_norm[i_channel][-1, :]
-                        # I[i_channel] = I_norm[i_channel][-1] + (I_norm[i_channel][-1, :] - I_norm[i_channel][-2, :]) * denom / v_norm[i_channel][-1, :]
 
             # convert back from I_norm to I
             I = [I_mean[i_channel] * (I[i_channel] - np.mean(I[i_channel]) + 1) for i_channel in range(n_channel)]
@@ -513,6 +657,29 @@ class OnlineFEM:
             # update error
             maxrelerr = np.max([np.max(np.abs(v_norm[i_channel][-1])) for i_channel in range(n_channel)])
 
+        # test if signs are correct otherwise restart
+        if not np.array([(np.sign(I[i_channel]) == I_sign[i_channel]).all() for i_channel in range(n_channel)]).all():
+            print(f"Final currents have wrong signs!")
+            print(f"I: { *I, }")
+            return None
+
+        # add optimal currents to CurrentEstimator to improve estimation in further iterations
+        electrode_pos = [_electrode_array.electrode_pos for _electrode_array in electrode.electrode_arrays]
+        ele_counter_channel = [0] * n_channel
+
+        for _electrode_array in electrode.electrode_arrays:
+            for _ele in _electrode_array.electrodes:
+                _ele.current_estimator.add_training_data(electrode_pos=np.hstack(electrode_pos),
+                                                         current=I[_ele.channel_id][ele_counter_channel[_ele.channel_id]])
+                ele_counter_channel[_ele.channel_id] += 1
+
+        self.logger.log(20, f"Optimal currents: { *I, }")
+
+        # reset to original currents
+        for _electrode_array in electrode.electrode_arrays:
+            for _ele in _electrode_array.electrodes:
+                _ele.current = _ele.current_init
+
         # import matplotlib.pyplot as plt
         # import matplotlib
         # matplotlib.use("Qt5Agg")
@@ -540,7 +707,6 @@ class OnlineFEM:
         #     plt.show()
 
         return np.squeeze(v)
-
 
     def solve(self, b):
         """

simnibs/simulation/region_of_interest.py
@@ -106,6 +106,8 @@ class RegionOfInterest:
         Indicator if points are lying inside model.
     idx : np.array of int [n_tet_mesh_required]
         Indices of tetrahedra, which are required for SPR
+    n_tet_mesh : int
+        Number of tetrahedra in the whole head mesh
     """
     def __init__(self, mesh, points, con=None, gradient=None, out_fill=0):
         """
@@ -123,6 +125,8 @@ class RegionOfInterest:
         # ensure that the nodes did not change
         assert mesh_cropped.nodes.nr == mesh.nodes.nr
         assert (mesh_cropped.nodes.node_coord == mesh.nodes.node_coord).all()
+
+        self.n_tet_mesh = mesh_cropped.elm.node_number_list.shape[0]
 
         # compute sF matrix
         self._get_sF_matrix(mesh_cropped, self.points, out_fill)
@@ -158,10 +162,14 @@ class RegionOfInterest:
         if dadt is None:
             # TES
             if dataType == 0:
-                fields = postp_mag(self.gradient, v, np.zeros((len(v), 3)), self.node_index_list, self.idx) # dadt should be in elements
+                fields = postp_mag(self.gradient, v, np.zeros((self.n_tet_mesh, 3)), self.node_index_list, self.idx) # dadt should be in elements
             else:
-                fields = postp(self.gradient, v, np.zeros((len(v), 3)), self.node_index_list, self.idx)
+                fields = postp(self.gradient, v, np.zeros((self.n_tet_mesh, 3)), self.node_index_list, self.idx)
+
             # fields = np.einsum('ijk,ij->ik', self.gradient, - (v * 1e3)[self.node_index_list])
+            # if dataType == 0:
+            #     fields = np.linalg.norm(fields, axis=1)
+
         else:
             # TMS
             if dataType == 0:

simnibs/utils/ellipsoid.py
@@ -4,7 +4,7 @@ import multiprocessing.pool
 from _functools import partial
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 from scipy.integrate import solve_ivp
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, minimize
 import warnings
 warnings.filterwarnings("ignore")
 warnings.filterwarnings('ignore', 'The iteration is not making good progress')
@@ -269,6 +269,34 @@ class Ellipsoid():
 
         return coords_eli
 
+    def cartesian2jacobi_system(self, x, coords):
+        """
+
+        :param x:
+        :return:
+        """
+        beta = x[0]
+        lam = x[1]
+
+        # B = self.E_x2 * np.cos(beta)**2 + self.E_e2 * np.sin(beta)**2
+        # L = self.E_x2 - self.E_e2 * np.cos(lam)**2
+        #
+        # f1 = self.radii[0] / self.E_x * np.sqrt(B) * np.cos(lam) - coords[0]
+        # f2 = self.radii[1] * np.cos(beta) * np.sin(lam) - coords[1]
+        # f3 = self.radii[2] / self.E_x * np.sin(beta) * np.sqrt(L) - coords[2]
+
+        v = self.radii[0] / np.sqrt(
+            1 - self.e_x2 * np.sin(beta) ** 2 - self.e_e2 * np.cos(beta) ** 2 * np.sin(lam) ** 2)
+
+        # cartesian coordinates in normalized space
+        f1 = v * np.cos(beta) * np.cos(lam) - coords[0]
+        f2 = v * (1 - self.e_e2) * np.cos(beta) * np.sin(lam) - coords[1]
+        f3 = v * (1 - self.e_x2) * np.sin(beta) - coords[2]
+
+        f = np.sqrt(f1**2 + f2**2 + f3**2)
+
+        return f
+
     def cartesian2jacobi(self, coords: np.ndarray, norm=False,  return_norm=False, return_normal=False) -> (np.ndarray,  np.ndarray):
         """
         Transforms cartesian coordinates on ellipsoid to spherical coordinates in Jacobi form and optionally
@@ -305,8 +333,34 @@ class Ellipsoid():
             coords = self.rot_org2norm(coords=coords)
 
         # calculate beta and lambda from normalized coordinates
-        beta = np.arctan((1-self.e_e2)/(1-self.e_x2) * coords[:, 2] / np.sqrt((1-self.e_e2)**2 * coords[:, 0]**2 + coords[:, 1]**2))
-        lam = np.arctan(1/(1-self.e_e2) * coords[:, 1] / coords[:, 0])
+        # beta = np.arctan((1-self.e_e2)/(1-self.e_x2) * coords[:, 2] / np.sqrt((1-self.e_e2)**2 * coords[:, 0]**2 + coords[:, 1]**2))
+        # lam = np.arctan(1/(1-self.e_e2) * coords[:, 1] / coords[:, 0])
+
+        # c0 = self.radii2[0] * self.radii2[1] + self.radii2[0]*self.radii2[2] + self.radii2[1]*self.radii2[2] \
+        #      - (self.radii2[1] + self.radii2[2]) * coords[:, 0] \
+        #      - (self.radii2[0] + self.radii2[2]) * coords[:, 1] \
+        #      - (self.radii2[0] + self.radii2[1]) * coords[:, 2]
+        # c1 = coords[:, 0]**2 + coords[:, 1]**2 + coords[:, 2]**2 - (self.radii2[0] + self.radii2[1] + self.radii2[2])
+        # if np.isnan(np.sqrt(c1**2 - 4 * c0)):
+        #     t2 = -c1 / 2.
+        # else:
+        #     t2 = (-c1 + np.sqrt(c1**2 - 4 * c0)) / 2.
+        # t1 = c0 / t2
+        # beta = np.arctan(np.sqrt((t1-self.radii2[2])/(self.radii2[1]-t1)))
+        # lam = np.arctan(np.sqrt((t2-self.radii2[1])/(self.radii2[0]-t2)))
+
+        beta = np.zeros(coords.shape[0])
+        lam = np.zeros(coords.shape[0])
+
+        for i, _coords in enumerate(coords):
+            res = minimize(self.cartesian2jacobi_system, (0, 0),
+                           method='Nelder-Mead',
+                           bounds=((-np.pi/2, np.pi/2), (-np.pi, np.pi)),
+                           args=_coords)
+
+            beta[i] = res.x[0]
+            lam[i] = res.x[1]
+
         coords_jacobi = np.hstack((beta[:, np.newaxis], lam[:, np.newaxis]))
 
         if return_normal:
@@ -655,15 +709,14 @@ class Ellipsoid():
             start = self.rot_org2norm(coords=start)
             # start = (self.rotmat.T @ (start - self.center).T).T
 
-        start_jacobi = self.cartesian2jacobi(coords=start, norm=True).flatten()
+        start_jacobi = self.cartesian2jacobi(coords=start, norm=norm).flatten()
 
-        # t1 = lambda beta: self.radii2[1]*np.sin(beta)**2 + self.radii2[2]*np.cos(beta)**2                   # eq. (13)
-        # t2 = lambda lam: self.radii2[0]*np.sin(lam)**2 + self.radii2[1]*np.cos(lam)**2                      # eq. (14)
-        # L = lambda lam: t2(lam) - self.radii2[2]                                                            # eq. (67)
-        # F = lambda lam, beta: t2(lam) - t1(beta)                                                            # eq. (68)
-        #
-        # B = lambda beta: self.E_x2/self.E_y2 * (self.radii2[1] - t1(beta)) + \
-        #                  self.E_e2/self.E_y2 * (t1(beta) - self.radii2[2])                                  # eq. (66)
+        t1 = lambda beta: self.radii2[1]*np.sin(beta)**2 + self.radii2[2]*np.cos(beta)**2                   # eq. (13)
+        t2 = lambda lam: self.radii2[0]*np.sin(lam)**2 + self.radii2[1]*np.cos(lam)**2                      # eq. (14)
+        L = lambda lam: t2(lam) - self.radii2[2]                                                            # eq. (67)
+        F = lambda lam, beta: t2(lam) - t1(beta)                                                            # eq. (68)
+        B = lambda beta: self.E_x2/self.E_y2 * (self.radii2[1] - t1(beta)) + \
+                         self.E_e2/self.E_y2 * (t1(beta) - self.radii2[2])                                  # eq. (66)
 
         # Initial conditions
         ################################################################################################################
@@ -676,11 +729,11 @@ class Ellipsoid():
 
         # Calculate Neumann boundary conditions
         H0 = self.H(start)
-        # H0_sqrt = np.sqrt(H0)
+        H0_sqrt = np.sqrt(H0)
         G0 = self.G(beta0, lambda0)
 
         # normal vector to start point on ellipsoid, eq. (58)
-        _, n = self.cartesian2ellipsoid(coords=start, return_normal=True)
+        _, n = self.cartesian2ellipsoid(coords=start, norm=norm, return_norm=True, return_normal=True)
         n = n.flatten()
         # n = np.zeros(3)
         # n[0] = x0 / H0_sqrt