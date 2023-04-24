tcd scaling fixes

simnibs/simulation/coil_numpy.py
@@ -211,7 +211,7 @@ def A_from_dipoles(d_moment, d_position, target_positions, eps=1e-3, direct='aut
         Precision. The default is 1e-3
     direct : bool
         Set to true to force using direct (naive) approach or False to force use of FMM.
-        If set to auto direct method is used for less than 300 dipoles which appears to be faster i these cases.
+        If set to auto direct method is used for less than 300 dipoles which appears to be faster in these cases.
         The default is 'auto'
 
     Returns
@@ -460,10 +460,6 @@ def _calculate_dadt_tcd(msh, coil, coil_matrix, didt, eps=1e-3, parameters=None)
     pos = msh.nodes[:] * 1e-3
     A = tcd_utils.get_Afield(coil, pos.T, affine=coil_matrix, dIdt=didt, eps=eps,
                              parameters=parameters)
-    print('test1')
-    print(parameters)
-    print(parameters is None)
-    print('test1')
     msh_stl = tcd_utils.get_coil_msh(coil, parameters=parameters, affine=np.identity(4))
     return mesh_io.NodeData(A), msh_stl
 
@@ -614,14 +610,16 @@ def set_up_tms_dAdt(msh, coil_file, coil_matrix, didt=1e6, fn_geo=None, fn_stl=N
     elif isinstance(coil_file, dict) or coil_file.endswith('.tcd'):
         dadt, msh_stl = _calculate_dadt_tcd(msh, coil_file,
                                             coil_matrix, didt)
-        idx = (msh.elm.elm_type == 2) & ((msh.elm.tag1 == 1005) |
-                                         (msh.elm.tag1 == 5))
-        msh_skin = msh.crop_mesh(elements=msh.elm.elm_number[idx])
-        msh_stl = _transform_coil_surface(msh_stl,
-                                          coil_matrix, msh_skin, add_logo)
-        mesh_io.write_geo_triangles(msh_stl.elm[:, :3] - 1, msh_stl.nodes[:],
-                                    'coil.geo', values=msh_stl.elm.tag1,
-                                    name='coil_casing', mode='ba')
+        if fn_geo is not None:
+            idx = (msh.elm.elm_type == 2) & ((msh.elm.tag1 == 1005) |
+                                             (msh.elm.tag1 == 5))
+            msh_skin = msh.crop_mesh(elements=msh.elm.elm_number[idx])
+            msh_stl = _transform_coil_surface(msh_stl,
+                                              coil_matrix, msh_skin, add_logo)
+            mesh_io.write_geo_triangles(msh_stl.elm[:, :3] - 1,
+                                        msh_stl.nodes[:],
+                                        fn_geo, values=msh_stl.elm.tag1,
+                                        name='coil_casing', mode='ba')
     elif coil_file.endswith('.ccd'):
         if FMM3D:
             dadt = _calculate_dadt_ccd_FMM(

simnibs/simulation/tcd_utils.py
@@ -31,6 +31,29 @@ def read_tcd(fn):
     return coil
 
 
+def write_tcd(coildict, fname):
+    coil = coildict.copy()
+    for i, cElm in enumerate(coil['elementList']):
+        if isinstance(coil['elementList'][i]['points'],np.ndarray):
+            coil['elementList'][i]['points'] = cElm['points'].tolist()
+        if isinstance(coil['elementList'][i]['values'],np.ndarray):
+            coil['elementList'][i]['values'] = cElm['values'].tolist()
+        for k in coil['coilModel'][i].keys():
+            try:
+                coil['coilModel'][i][k] = coil['coilModel'][i][k].tolist()
+            except:
+                pass
+
+    for j, deform in enumerate(coil['deformationList']):
+        for k in deform.keys():
+            if isinstance(deform[k], np.ndarray):
+                coil['deformationList'][j][k] = deform[k].tolist()
+
+    json_tcd = json.dumps(coil)
+    with open(fname, 'w') as fid:
+        fid.write(json_tcd)
+
+
 def rot2p(p1, p2, a, unit='deg', affine=None):
     p1 = np.asanyarray(p1, dtype='float64')
     p2 = np.asanyarray(p2, dtype='float64')
@@ -135,19 +158,14 @@ def get_params(coil):
 
 def transform_coil(coil, parameters,
                    fieldnames=['points', 'minDistancePoints']):
+    parameters = np.asanyarray(parameters)
     for j, cElm in enumerate(coil['elementList']):
-        if parameters is None:
-            R = combine_transforms(coil['deformationList'],
-                                   0,
-                                   idx=cElm['deformations'])
-        else:
-            R = combine_transforms(coil['deformationList'],
-                                   parameters[cElm['deformations']],
-                                   idx=cElm['deformations'])
+        R = combine_transforms(coil['deformationList'],
+                               parameters[cElm['deformations']],
+                               idx=cElm['deformations'])
         cElm['points'] = transform_points(cElm['points'], R)
         if 'values' in cElm:
             cElm['values'] = transform_vectors(cElm['values'], R)
-
         if 'coilModel' in coil and len(coil['coilModel']) > j:
             for k, f in enumerate(fieldnames):
                 if f in coil['coilModel'][j]:
@@ -157,7 +175,7 @@ def transform_coil(coil, parameters,
 
 def get_Afield(coil, positions, parameters=None, affine=None, dIdt=1.0, eps=1e-3):
     # positions in SI units (meters)
-    # coil positions are converted to meters *1e-3
+    # coil wire positions are converted to meters *1e-3
     points = []
     values = []
     if affine is None:
@@ -187,20 +205,19 @@ def get_Afield(coil, positions, parameters=None, affine=None, dIdt=1.0, eps=1e-3
     for i, p in enumerate(points):
         # dipole case
         if coil['elementList'][i]['type'] == 1:
-            A += coil_numpy.A_from_dipoles(values[k], p*1e-3, positions.T,
-                                           eps=eps)
+            A += dIdt * coil_numpy.A_from_dipoles(values[k], p*1e-3,
+                                                  positions.T, eps=eps)
             k += 1
         # line integral with directions
         elif coil['elementList'][i]['type'] == 2:
             A += coil_numpy.A_biot_savart_path_fmm(p.T*1e-3,
-                                                   positions, values[k].T,
-                                                   eps=eps).T
+                                                   positions, values[k].T*1e-3,
+                                                   eps=eps, I=dIdt).T
             k += 1
         # line integral without directions
         elif coil['elementList'][i]['type'] == 3:
             A += coil_numpy.A_biot_savart_path_fmm(p.T*1e-3, positions,
-                                                   eps=eps).T
-    A *= dIdt
+                                                   eps=eps, I=dIdt).T
     return A
 
 