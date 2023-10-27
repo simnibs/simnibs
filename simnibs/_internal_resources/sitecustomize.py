63c417eb75188a09cc51467cc16a0152964daf67
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2023-10-27 13:16:43 +0200

    Fix dll path in sitecustomize, added dll search path using os module, changed  to  in windows azure pipeline

--- simnibs/_internal_resources/sitecustomize.py
+++ simnibs/_internal_resources/sitecustomize.py
@@ -6,10 +6,10 @@
 import os
 
 prefix = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', '..'))
-#  Notice: this is where conda stores DLLs. If that changes in the future, this here should also change
+# Notice: this is where conda stores DLLs. If that changes in the future, this here should also change
 # From conda-pack/conda_pack/scripts/windows/activate.bat
 os.environ["PATH"] = os.pathsep.join([
-    os.path.join(prefix, 'Library', 'mingw-w64'),
+    os.path.join(prefix, 'Library', 'mingw-w64', 'bin'),
     os.path.join(prefix, 'Library', 'usr', 'bin'),
     os.path.join(prefix, 'Library', 'bin')
 ]) + os.pathsep + os.environ["PATH"]
