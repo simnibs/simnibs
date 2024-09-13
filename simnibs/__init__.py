aaeaef30d31cc41f8e8efb35e4de13d344757022
Jesper Duemose Nielsen <jesperdn@drcmr.dk>
2024-09-13 13:59:19 +0200

    Removed PETSc headers and libraries and adjusted setup and init accordingly (also removed some libs that were previously used with CGAL)

--- simnibs/__init__.py
+++ simnibs/__init__.py
@@ -1,26 +1,7 @@
 import os
 import sys
 SIMNIBSDIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-# Add the external/lib/win folder to system path so that the dlls there can be found
-if sys.platform == 'win32':
-    os.environ['PATH'] = os.pathsep.join([
-        os.path.join(SIMNIBSDIR, 'external', 'lib', 'win'),
-        os.environ['PATH']
-    ])
-    # windows only: starting from python 3.8, the ctypes module only searches
-    # for DLLs in certain (trusted) locations, hence we need to add this
-    # explicitly to pick up the runtime dependencies for petsc
-    os.add_dll_directory(os.path.join(SIMNIBSDIR, "external", "lib", "win"))
-elif sys.platform == 'linux':
-    os.environ['PATH'] = os.pathsep.join([
-        os.path.join(SIMNIBSDIR, 'external', 'lib', 'linux'),
-        os.environ['PATH']
-    ])
-elif sys.platform == 'darwin':
-    os.environ['PATH'] = os.pathsep.join([
-        os.path.join(SIMNIBSDIR, 'external', 'lib', 'osx'),
-        os.environ['PATH']
-    ])
+
 from ._version import __version__
 from .mesh_tools import mesh_io
 from .mesh_tools.mesh_io import read_msh
