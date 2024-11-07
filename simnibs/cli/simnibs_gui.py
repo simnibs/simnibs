a3a8c2f89eaa5c66285bba65beb7073f7fd87975
Kristoffer <stoffer@drcmr.dk>
2024-11-07 10:49:38 +0100

    pyopengl_platform=omesa workaround for linux

--- simnibs/cli/simnibs_gui.py
+++ simnibs/cli/simnibs_gui.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import subprocess
-from simnibs.GUI.main_gui import start_gui
 
 big_sur = False
 if sys.platform == 'darwin':
@@ -11,9 +10,16 @@ if sys.platform == 'darwin':
             big_sur = True
     except:
         print('Mac OS sw_vers failed.')
+elif sys.platform == 'linux':
+    os.environ['PYOPENGL_PLATFORM'] = 'osmesa'
+
 if big_sur:
     print('Mac OS X Big Sur detected, setting QT_MAC_WANTS_LAYER=1 flag.')
     os.environ['QT_MAC_WANTS_LAYER'] = '1'
+
+
+from simnibs.GUI.main_gui import start_gui
+    
 
 def main():
     start_gui(sys.argv)
