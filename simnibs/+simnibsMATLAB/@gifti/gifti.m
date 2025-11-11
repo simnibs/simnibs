7615c586d54089b03586e35e294243148e0d2e36
Santtu Söderholm <santtu.soderholm@tuni.fi>
2025-11-11 22:16:27 +0200

    Start implementing a proper constructor for the gifti class
    This is to make it compatible with MATLAB's namespaces.
    
    Added properties with beginnings of MATLAB property validation such that
    the code generates an empty gifti object when the constructor is given
    no arguments. However, it is unclear how to test this properly.
    
    Ispecially interesting is the struct branch of the constructor, where
    the given struct contains the fields 'metadata', 'label' and 'data'.
    It seems that the original behaviour would have resulted in infinite
    recursion (without testing). Now the object is just returned as-is.
    
    You will want to disable whitespace diffs when reviewing this one.

--- simnibs/+simnibsMATLAB/@gifti/gifti.m
+++ simnibs/+simnibsMATLAB/@gifti/gifti.m
@@ -1,4 +1,4 @@
-function this = gifti(varargin)
+classdef gifti
 % GIfTI Geometry file format class
 % Geometry format under the Neuroimaging Informatics Technology Initiative
 % (NIfTI):
@@ -7,123 +7,152 @@ function this = gifti(varargin)
 %__________________________________________________________________________
 % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
 
-% Guillaume Flandin
-% $Id: gifti.m 7621 2019-06-20 16:58:59Z guillaume $
+    properties
 
-switch nargin
-    
-    case 0
-        this = giftistruct;
-        this = class(this,'gifti');
-        
-    case 1
-        if isa(varargin{1},'gifti')
-            this = varargin{1};
-            
-        elseif isstruct(varargin{1})
-            f       = {'faces', 'face', 'tri' 'vertices', 'vert', 'pnt', 'cdata', 'indices'};
-            ff      = {'faces', 'faces', 'faces', 'vertices', 'vertices', 'vertices', 'cdata', 'indices'};
-            [c, ia] = intersect(f,fieldnames(varargin{1}));
-            if ~isempty(c)
-                this = gifti;
-                for i=1:length(c)
-                    this = subsasgn(this,...
-                        struct('type','.','subs',ff{ia(i)}),...
-                        varargin{1}.(c{i}));
-                end
-                if isfield(varargin{1},'mat')
-                    this = subsasgn(this,...
-                        struct('type','.','subs','mat'),...
-                        varargin{1}.mat);
-                end
-            elseif isempty(setxor(fieldnames(varargin{1}),...
-                    {'metadata','label','data'}))
-                this = class(varargin{1},'gifti');
-            else
-                error('[GIFTI] Invalid structure.');
-            end
-            
-        elseif ishandle(varargin{1})
-            this = struct('vertices',get(varargin{1},'Vertices'), ...
-                          'faces',   get(varargin{1},'Faces'));
-            if ~isempty(get(varargin{1},'FaceVertexCData'))
-                  this.cdata = get(varargin{1},'FaceVertexCData');
-            end
-            this = gifti(this);
-            
-        elseif isnumeric(varargin{1})
-            this = gifti;
-            this = subsasgn(this,...
-                struct('type','.','subs','cdata'),...
-                varargin{1});
-            
-        elseif iscell(varargin{1}) && numel(varargin{1}) == 1 && ...
-            isnumeric(varargin{1}{1})
-            this = gifti;
-            for i=1:size(varargin{1}{1},2)
-                this.data{i}.metadata = struct([]);
-                this.data{i}.space    = [];
-                this.data{i}.attributes.Intent = 'NIFTI_INTENT_NONE';
-                this.data{i}.attributes.DataType = 'NIFTI_TYPE_FLOAT32';
-                this.data{i}.attributes.Dim = size(varargin{1}{1},1);
-                this.data{i}.data     = single(varargin{1}{1}(:,i));
-            end
-            
-        elseif ischar(varargin{1})
-            if size(varargin{1},1)>1
-                this = gifti(cellstr(varargin{1}));
-                return;
-            end
-            [p,n,e] = fileparts(varargin{1});
-            if strcmpi(e,'.mat')
-                try
-                    this = gifti(load(varargin{1}));
-                catch
-                    error('[GIFTI] Loading of file %s failed.', varargin{1});
-                end
-            elseif ismember(lower(e),{'.asc','.srf','.mgh','.mgz','.pial',...
-                    '.white','.inflated','.nofix','.orig','.smoothwm',...
-                    '.sphere','.reg','.surf','.curv','.area','.sulc','.annot'})
-                this = freesurfer_read(varargin{1});
-                this = gifti(this);
-            elseif strcmpi(e,'.vtk')
-                this = mvtk_read(varargin{1});
-                this = gifti(this);
-            elseif strcmpi(e,'.obj')
-                this = obj_read(varargin{1});
-                this = gifti(this);
-            elseif strcmpi(e,'.ply')
-                this = ply_read(varargin{1});
-                this = gifti(this);
-            elseif strcmpi(e,'.off')
-                this = off_read(varargin{1});
-                this = gifti(this);
-            elseif strcmpi(e,'.stl')
-                this = stl_read(varargin{1});
-                this = gifti(this);
-            elseif strcmpi(e,'.mz3')
-                this = mz3_read(varargin{1});
-                this = gifti(this);
-            else
-                this = gifti_read(varargin{1},giftistruct);
-                this = class(this,'gifti');
-            end
-            
-        elseif iscellstr(varargin{1})
-            fnames = varargin{1};
-            this(numel(fnames)) = giftistruct;
-            this = class(this,'gifti');
-            for i=1:numel(fnames)
-                this(i) = gifti(fnames{i});
-            end
-            
-        else
-            error('[GIFTI] Invalid object construction.');
-        end
-        
-    otherwise
-        error('[GIFTI] Invalid object construction.');
-end
+        metadata (1,1) struct = struct % TODO: short documentation here.
+
+        label (1,1) struct = struct % TODO: short documentation here.
+
+        data (1,:) cell = struct % TODO: short documentation here.
+
+        mat % TODO: add size, class and default value restrictions here.
+
+        faces % TODO: add size, class and default value restrictions here.
+
+        vertices % TODO: add size, class and default value restrictions here.
+
+        cdata (1,1) struct = struct % TODO: short documentation here.
+
+        indices % TODO: add size, class and default value restrictions here.
+
+    end % properties
+
+    methods
+
+        function this = gifti(varargin)
+        % GIfTI Geometry file format class
+        % Geometry format under the Neuroimaging Informatics Technology Initiative
+        % (NIfTI):
+        %                 http://www.nitrc.org/projects/gifti/
+        %                      http://nifti.nimh.nih.gov/
+        %__________________________________________________________________________
+        % Copyright (C) 2008 Wellcome Trust Centre for Neuroimaging
+
+        % Guillaume Flandin
+        % $Id: gifti.m 7621 2019-06-20 16:58:59Z guillaume $
+
+            switch nargin
+
+                case 0
+
+                    this = simnibsMATLAB.gifti(giftistruct) ;
+
+                case 1
+                    if isa(varargin{1},'simnibsMATLAB.gifti')
+                        this = varargin{1};
+
+                    elseif isstruct(varargin{1})
+                        f       = {'faces', 'face', 'tri' 'vertices', 'vert', 'pnt', 'cdata', 'indices'};
+                        ff      = {'faces', 'faces', 'faces', 'vertices', 'vertices', 'vertices', 'cdata', 'indices'};
+                        [c, ia] = intersect(f,fieldnames(varargin{1}));
+                        if ~isempty(c)
+
+                            for i=1:length(c)
+                                fieldName = ff{ia(i)} ;
+                                this.(fieldName) = varargin{1}.(c{i}) ;
+                            end
+                            if isfield(varargin{1},'mat')
+                                this.mat = varargin{1}.mat ;
+                            end
+                        elseif isempty(setxor(fieldnames(varargin{1}), {'metadata','label','data'}))
+                            this = this ;
+                        else
+                            error('[GIFTI] Invalid structure.');
+                        end
+
+                    elseif ishandle(varargin{1})
+
+                        this = struct('vertices',get(varargin{1},'Vertices'), ...
+                                      'faces',   get(varargin{1},'Faces'));
+                        if ~isempty(get(varargin{1},'FaceVertexCData'))
+                              this.cdata = get(varargin{1},'FaceVertexCData');
+                        end
+                        this = simnibsMATLAB.gifti(this);
+
+                    elseif isnumeric(varargin{1})
+
+                        this = simnibsMATLAB.gifti;
+                        this.cdata = varargin{1};
+
+                    elseif iscell(varargin{1}) && numel(varargin{1}) == 1 && isnumeric(varargin{1}{1})
+                        this = simnibsMATLAB.gifti;
+                        for i=1:size(varargin{1}{1},2)
+                            this.data{i}.metadata = struct([]);
+                            this.data{i}.space    = [];
+                            this.data{i}.attributes.Intent = 'NIFTI_INTENT_NONE';
+                            this.data{i}.attributes.DataType = 'NIFTI_TYPE_FLOAT32';
+                            this.data{i}.attributes.Dim = size(varargin{1}{1},1);
+                            this.data{i}.data     = single(varargin{1}{1}(:,i));
+                        end
+
+                    elseif ischar(varargin{1})
+
+                        if size(varargin{1},1)>1
+                            this = simnibsMATLAB.gifti(cellstr(varargin{1}));
+                            return;
+                        end
+                        [~,~,e] = fileparts(varargin{1});
+                        if strcmpi(e,'.mat')
+                            try
+                                this = simnibsMATLAB.gifti(load(varargin{1}));
+                            catch
+                                error('[GIFTI] Loading of file %s failed.', varargin{1});
+                            end
+                        elseif ismember(lower(e),{'.asc','.srf','.mgh','.mgz','.pial',...
+                                '.white','.inflated','.nofix','.orig','.smoothwm',...
+                                '.sphere','.reg','.surf','.curv','.area','.sulc','.annot'})
+                            this = freesurfer_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        elseif strcmpi(e,'.vtk')
+                            this = mvtk_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        elseif strcmpi(e,'.obj')
+                            this = obj_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        elseif strcmpi(e,'.ply')
+                            this = ply_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        elseif strcmpi(e,'.off')
+                            this = off_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        elseif strcmpi(e,'.stl')
+                            this = stl_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        elseif strcmpi(e,'.mz3')
+                            this = mz3_read(varargin{1});
+                            this = simnibsMATLAB.gifti(this);
+                        else
+                            this = gifti_read(varargin{1},giftistruct);
+                            this = simnibsMATLAB.gifti(this);
+                        end
+
+                    elseif iscellstr(varargin{1})
+                        fnames = varargin{1};
+                        this(numel(fnames)) = simnibsMATLAB.gifti(giftistruct);
+                        for i=1:numel(fnames)
+                            this(i) = simnibsMATLAB.gifti(fnames{i});
+                        end
+
+                    else
+                        error('[GIFTI] Invalid object construction.');
+                    end
+
+                otherwise
+                    error('[GIFTI] Invalid object construction.');
+            end % switch
+        end % function
+    end % methods
+end % classdef
 
 %==========================================================================
 function s = giftistruct
@@ -146,3 +175,5 @@ s = struct(...
             'data',       {} ...
         ) ...
     );
+
+end % function
